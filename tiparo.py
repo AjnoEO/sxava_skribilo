8a048493498bb5383ef570279b0cd8345b1264d0
Ajno <boyko.artiom@gmail.com>
2024-04-09 11:54:10 +0300

    Forigo de PNG, aldono de SVG de 'an' kaj 'on'

--- tiparo.py
+++ tiparo.py
@@ -1,13 +1,13 @@
 import json, ziafont
 
 with open("datumoj.json", mode="r", encoding="utf-8") as f:
-  data = json.load(f)
-  UNICODE = data['UNIKODO']
+	data = json.load(f)
+	UNICODE = data['Unikodo']
 
 font = ziafont.Font('seguihis.ttf')
 
 for letter in UNICODE:
-  print('>', letter)
-  with open(f"literoj/{letter}.svg", mode="w", encoding="utf-8") as f:
-    f.write(font.text(UNICODE[letter]).svg())
-  print(f'  La litero «{letter}» aldoniĝis')
+	print('>', letter)
+	with open(f"literoj/{letter}.svg", mode="w", encoding="utf-8") as f:
+		f.write(font.text(UNICODE[letter]).svg())
+	print(f'  La litero «{letter}» aldoniĝis')
